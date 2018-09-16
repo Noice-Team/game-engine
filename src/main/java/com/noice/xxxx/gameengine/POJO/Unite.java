002ebc0e3598ffe909e7dce0b691d5461bf9a5d0
Pierric <Pierric@192.168.1.62>
2018-09-16 17:56:17 +0200

    ajout carac des unites

--- src/main/java/com/noice/xxxx/gameengine/POJO/Unite.java
+++ src/main/java/com/noice/xxxx/gameengine/POJO/Unite.java
@@ -10,6 +10,15 @@ public class Unite {
 	private Joueur joueur;
 	private Case emplacement;
 	private List<Case> chemin;
+	
+	private int precision;
+	private int esquive;
+	private int nombreMaxPV;
+	private int nombrePVRestant;
+	private int armure;
+	private int degatsMin;
+	private int degatsMax;
+	
 	public Joueur getJoueur() {
 		return joueur;
 	}
@@ -28,6 +37,49 @@ public class Unite {
 	public void setChemin(List<Case> chemin) {
 		this.chemin = chemin;
 	}
+	public int getPrecision() {
+		return precision;
+	}
+	public void setPrecision(int precision) {
+		this.precision = precision;
+	}
+	public int getEsquive() {
+		return esquive;
+	}
+	public void setEsquive(int esquive) {
+		this.esquive = esquive;
+	}
+	public int getNombreMaxPV() {
+		return nombreMaxPV;
+	}
+	public void setNombreMaxPV(int nombreMaxPV) {
+		this.nombreMaxPV = nombreMaxPV;
+	}
+	public int getNombrePVRestant() {
+		return nombrePVRestant;
+	}
+	public void setNombrePVRestant(int nombrePVRestant) {
+		this.nombrePVRestant = nombrePVRestant;
+	}
+	public int getArmure() {
+		return armure;
+	}
+	public void setArmure(int armure) {
+		this.armure = armure;
+	}
+	public int getDegatsMin() {
+		return degatsMin;
+	}
+	public void setDegatsMin(int degatsMin) {
+		this.degatsMin = degatsMin;
+	}
+	public int getDegatsMax() {
+		return degatsMax;
+	}
+	public void setDegatsMax(int degatsMax) {
+		this.degatsMax = degatsMax;
+	}
+
 	public boolean canWalkIn(Case case1) {
 		// TODO Auto-generated method stub
 		return true;
