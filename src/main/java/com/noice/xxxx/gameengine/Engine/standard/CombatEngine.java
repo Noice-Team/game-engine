9a626b6d389835cbb812dcc9b89fb3e91cfefd02
Pierric <Pierric@192.168.1.62>
2018-09-16 19:33:16 +0200

    correctif merge

--- src/main/java/com/noice/xxxx/gameengine/Engine/standard/CombatEngine.java
+++ src/main/java/com/noice/xxxx/gameengine/Engine/standard/CombatEngine.java
@@ -8,11 +8,8 @@ import java.util.Random;
 import org.springframework.stereotype.Service;
 
 import com.noice.xxxx.gameengine.Engine.ICombatEngine;
-<<<<<<< HEAD
 import com.noice.xxxx.gameengine.POJO.Joueur;
-=======
 import com.noice.xxxx.gameengine.POJO.Case;
->>>>>>> refs/heads/master
 import com.noice.xxxx.gameengine.POJO.Unite;
 
 @Service
